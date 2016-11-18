3f25fb229a7cfd53bc7f743305024fe4c317e75c
Thang Trinh <thangtrinh@MacBook-Pro.local>
2016-11-18 16:04:32 +0700

    add es url

--- scrape_vne/middlewares.py
+++ scrape_vne/middlewares.py
@@ -6,10 +6,12 @@ import os
 
 ES_PORT = os.getenv('ES_PORT', 8889)
 MAX_TTL = os.getenv('MAX_TTL', 30)
+ES_PORT = os.getenv('ELASTICSEARCH_PORT', 443)
+ES_URL = os.getenv('ELASTICSEARCH_URL', 'https://wji4cg72wu:1lr4slrzuz@acme-development-8779249584.us-east-1.bonsai.io/')
 
 class DupFilterMiddleware(object):
     def __init__(self):
-        self.es = Elasticsearch([{'host': 'localhost', 'port': ES_PORT}])
+        self.es = Elasticsearch([{'host': ES_URL, 'port': ES_PORT}])
         print "DupFilterMiddleware Initialize elasticsearch connection"
 
     def process_request(self, request, spider):
